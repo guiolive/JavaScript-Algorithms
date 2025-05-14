74c3a52a8b04f8b63d249bd79cf2acfbdd226d30
“oliguitos” <202501273653@alunos.estacio.br>
2025-05-14 19:36:05 -0300

    binary search

--- 17_binarySearch.js
+++ 17_binarySearch.js
@@ -1,24 +1,23 @@
 function binarySearch(arr, target) {
-    let leftIndex = 0; // indice esquerdo
-    let rightIndex = arr.lenght - 1; // indice direito ou final, tamanho do array, 5 por exemplo - 1 = 4 == 10.
+    let leftIndex = 0;
+    let rightIndex = arr.length - 1;
 
-    while (leftIndex <= rightIndex) { // enquanto indice esquerdo for menor ou igual que o indice final, faca
-        let middleIndex = Math.floor(leftIndex + rightIndex / 2); // procurando o meio com arredondamento.
+    while (leftIndex <= rightIndex) {
+        let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
 
-        if (target === arr[middleIndex]) { // se algo eh igual ao meio entao retorna o meio, que seria 4 se fosse V.
+        if (target === arr[middleIndex]) {
             return middleIndex;
-        } else if (middleIndex < target) { // se nao, verifica se o meio eh menor ou maior que o alvo.
-            rightIndex = middleIndex; // se for menor o meio vira o fim da arrayf
         }
-
-
+        if (target < arr[middleIndex]) {
+            rightIndex = middleIndex - 1;
+        } else {
+            leftIndex = middleIndex + 1;
+        }
     }
-
-
+    return -1
 }
 
 
 console.log(binarySearch([-5, 2, 4, 6, 10], 10)) // 4
 console.log(binarySearch([-5, 2, 4, 6, 10], 6)) // 3 
 console.log(binarySearch([-5, 2, 4, 6, 10], 20))// -1
-s
